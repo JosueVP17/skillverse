d09a3bb4c1f32eaa28a852f76956fff23d187782
Naomi <en.gallardolicea@ugto.mx>
2025-11-25 16:01:32 -0600

    [CREATED]: Se creó la lógica de usuario: registrar, login, logout, delete, update.

---
+++ backend/src/interfaces/usuario.interface.js
@@ -0,0 +1,11 @@
+export default class UsuarioInterface {
+    constructor({
+        nombre, apaterno, amaterno, edad, email, password}) {
+            this.nombre = nombre
+            this.apaterno = apaterno
+            this.amaterno = amaterno
+            this.edad = edad
+            this.email = email
+            this.password = password
+    }
+}
